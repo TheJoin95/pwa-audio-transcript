1e9921aed8f67a296af5ed26da833bac0c33b929
thejoin <miki.lombi@gmail.com>
2019-12-26 16:36:08 +0100

    add console remote log

--- sw.js
+++ sw.js
@@ -6,17 +6,16 @@ addEventListener('activate', () => {
   clients.claim();
 });
 
-addEventListener('fetch', (event) => {
+/*addEventListener('fetch', (event) => {
   if (event.request.method !== 'POST') return;
   
   event.respondWith(Response.redirect('/pwa-audio-transcript/'));
   
   event.waitUntil(async function () {
     const data = await event.request.formData();
-    console.re.log(event);
     const client = await self.clients.get(event.resultingClientId);
-    console.re.log(client);
     const file = data.get('file');
     client.postMessage({ file });
   }());
 });
+*/
