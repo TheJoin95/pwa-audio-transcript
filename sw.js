150ca71e53221f91d863740ac34b63fba7fe9198
thejoin <miki.lombi@gmail.com>
2019-12-26 12:47:26 +0100

    fix cors

---
+++ sw.js
@@ -0,0 +1,20 @@
+addEventListener('install', () => {
+  skipWaiting();
+});
+
+addEventListener('activate', () => {
+  clients.claim();
+});
+
+/*addEventListener('fetch', (event) => {
+  if (event.request.method !== 'POST') return;
+  
+  event.respondWith(Response.redirect('./'));
+  
+  event.waitUntil(async function () {
+    const data = await event.request.formData();
+    const client = await self.clients.get(event.resultingClientId);
+    const file = data.get('file');
+    client.postMessage({ file });
+  }());
+});*/
